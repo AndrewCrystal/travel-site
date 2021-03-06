010d9325b8b5e9e9d4d85ce1364c72d602d37a24
Andrew <andrewcrystal@gmail.com>
2021-03-06 17:41:03 +0000

    Completed modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,17 +4,51 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 
+//removed as part of code splitting as might not be needed
+//import Modal from './modules/Modal';
+
 if (module.hot) {
 	module.hot.accept();
 }
 
-let stickyHeader = new StickyHeader();
+//removed as part of code splitting as might not be needed
+//new Modal();
+//as this isn't used / referred to anywhere else don't need a variable
+//would need to be in a variable if the different modules were interacting
+//let stickyHeader = new StickyHeader();
+new StickyHeader();
 
 //can reuse the same reveal class in this way
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
 
-let mobileMenu = new MobileMenu();
+//as this isn't used / referred to anywhere else don't need a variable
+//let mobileMenu = new MobileMenu();
+new MobileMenu();
+
+let modal; //global scope
+
+//Load modal code if called
+document.querySelectorAll('.open-modal').forEach(el => {
+	el.addEventListener('click', e => {
+		e.preventDefault();
+		//check if modal file has already been loaded
+		if (typeof modal == 'undefined') {
+			//this returns a promise so can use .then to see what to do next and .catch in case of problems
+			//the comment in the function below dictates what webpack calls the file when it's loaded
+			import(/* webpackChunkName: "modal" */ './modules/Modal')
+				.then(x => {
+					//x is the imported file and we need the .default due to how webpack will import it
+					modal = new x.default();
+					//open modal but give it 20ms to create object and inject html
+					setTimeout(() => modal.openTheModal(), 20);
+				})
+				.catch(() => console.log('There was problem.'));
+		} else {
+			modal.openTheModal();
+		}
+	});
+});
 
 /* Lesson example code below this line */
 
